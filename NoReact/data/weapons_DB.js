13fbe318a9c2383114faf3b6563bd400c8a9a0ae
Anshakov Kirill Igorevich <kkk.rbhbkk@bk.ru>
2025-03-30 13:43:06 +0300

    add react try

---
+++ NoReact/data/weapons_DB.js
@@ -0,0 +1,13 @@
+weapons_DB = [
+    { type: "Средний Пистолет", skill:"Пистолеты" , damage:"2d6", ammoCount:"12(С Пистолет) ", dps:"2", hand:"1", isHide:"Да" , cost:"50eb(Ценное)" , alt: []},
+    { type: "Тяжелый Пистолет", skill:"Пистолеты" , damage:"3d6", ammoCount:"8(Т Пистолет) ", dps:"2", hand:"1", isHide:"Да" , cost:"100eb(Премиум)", alt: []},
+    { type: "Оч. Тяжелый пистолет", skill:"Пистолеты" , damage:"4d6", ammoCount:"8(ОТ Пистолет)", dps:"1", hand:"1", isHide:"Нет" , cost:"100eb(Премиум)", alt: "Нет"},
+    { type: "ПП", skill:"Пистолеты" , damage:"2d6", ammoCount:"30(С пистолет)", dps:"1", hand:"1", isHide:"Да" , cost:"100eb(Премиум)", alt: ["Автоогонь (3)", "Огонь на подавление"]},
+    { type: "Тяжелый ПП", skill:"Пистолеты" , damage:"3d6", ammoCount:"40(Т пистолет) ", dps:"1", hand:"1", isHide:"Нет" , cost:"100eb(Премиум)", alt: ["Автоогонь (3)", "Огонь на подавление"]},
+    { type: "Дробовик", skill:"Тактическое оружие" , damage:"5d6", ammoCount:"4(Жакан) ", dps:"1", hand:"2", isHide:"Нет" , cost:"500eb(Дорогое)", alt: ["Выстрел дробью"]},
+    { type: "Штурмовая Винтовка", skill:"Тактическое оружие" , damage:"5d6", ammoCount:"25(Винтовка)", dps:"1", hand:"2", isHide:"Нет" , cost:"500eb(Дорогое)", alt: ["Автоогонь (4)", "Огонь на подавление"]},
+    { type: "Снайперская Винтовка ", skill:"Тактическое оружие" , damage:"5d6", ammoCount:"4(Винтовка) ", dps:"1", hand:"2", isHide:"Нет" , cost:"500eb(Дорогое)", alt:[]},
+    { type: "Луки и Арбалеты", skill:"Стрельба из лука" , damage:"4d6", ammoCount:"Нет(Стрелы)", dps:"1", hand:"2", isHide:"Нет" , cost:"100eb(Премиум)", alt: []},
+    { type: "Гранатомёт", skill:"Оружие кр. калибра" , damage:"6d6", ammoCount:"2(Граната)", dps:"1", hand:"2", isHide:"Нет" , cost:"500eb(Дорогое)", alt: ["Взрывы"]},
+    { type: "Ракетная Установка", skill:"Оружие кр. калибра" , damage:"8d6", ammoCount:"1(Ракета) ", dps:"1", hand:"2", isHide:"Нет" , cost:"500eb(Дорогое)", alt: ["Взрывы"]} 
+]
